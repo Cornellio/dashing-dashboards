d72696c45ce1b1ad20a430296e7dba43af28f009
cornellio <peter.cornell@me.com>
2015-01-15 15:21:45 -0800

    Add code that loops over arbitrary # of stored values, update function names, style improvements

--- webstats/jobs/api_tam_stats.sh
+++ webstats/jobs/api_tam_stats.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # @version $Id: push_api_tam_stats.sh 934 2014-12-22 03:27:50Z pete.cornell $
 #
-# Push Sabre TAM pool usage data to dashboard server http://dashing.virginam.com
+# Push Sabre TAM pool usage data to dashing server
 #
-## Example curl to plot a sequence of data points:
+## Example curl to plot a sequence of data points to graph widget:
 # curl -d '{ "AUTH_TOKEN": "mingle#trip", "points": [{ "x" :1, "y" :250 }, { "x" :2, "y" :1120 }, { "x" :3, "y" :822 } ] }' http://dashing/widgets/webconnections
-#
-# TODO: make loop for post_data
+
+[ $# -ne 2 ] && { echo "usage: $(basename $0) [loginKey] [# of data points to plot]" && exit 1 ;}
 
 LOGINKEY="$1"
+DATAPOINTS="$2"
 DASHING_SERVER="http://dashing.virginam.com"
 AUTH_TOKEN="mingle#trip"
 TARGET_WIDGET="sabresessions"
@@ -16,59 +17,56 @@ SERVER_CONNECTION="${DASHING_SERVER}/widgets/${TARGET_WIDGET}"
 DATA_VIEW="points"
 SESSION_HISTORY_FILE="/home/vajobs/bin/dashboards/webstats/jobs/api_tam_stats.history"
 
-# Lookup TAM usage via api call and append to file
-get_sabresessions () {
+get_tam_pool_usage () {
+  # Lookup TAM pool usage via api call and append to history file
+
   connections=$( curl -sL https://www.virginamerica.com/api/v0/session/usage?loginKey=${LOGINKEY} | awk '{print $54}'  )
   echo $connections >> $SESSION_HISTORY_FILE
+
 }
 
-# Send data points to dashboard server
 update_widget () {
+  # Send data points to dashboard server
+
   echo "Sending ${#} values to ${TARGET_WIDGET} widget:  ${*}"
-  # Data points to post must match $value_len ##
-  post_data="[ { \"x\": 1, \"y\":  ${1} }, \
-    { \"x\": 2, \"y\":  ${2} }, \
-    { \"x\": 3, \"y\":  ${3} }, \
-    { \"x\": 4, \"y\":  ${4} }, \
-    { \"x\": 5, \"y\":  ${5} }, \
-    { \"x\": 6, \"y\":  ${6} }, \
-    { \"x\": 7, \"y\":  ${7} }, \
-    { \"x\": 8, \"y\":  ${8} }, \
-    { \"x\": 9, \"y\":  ${9} }, \
-    { \"x\": 10, \"y\": ${10} }, \
-    { \"x\": 11, \"y\": ${11} }, \
-    { \"x\": 12, \"y\": ${12} }, \
-    { \"x\": 13, \"y\": ${13} }, \
-    { \"x\": 14, \"y\": ${14} }, \
-    { \"x\": 15, \"y\": ${15} }, \
-    { \"x\": 16, \"y\": ${16} }, \
-    { \"x\": 17, \"y\": ${17} }, \
-    { \"x\": 18, \"y\": ${18} }, \
-    { \"x\": 19, \"y\": ${19} }, \
-    { \"x\": 20, \"y\": ${20} }, \
-    { \"x\": 21, \"y\": ${21} }, \
-    { \"x\": 22, \"y\": ${22} }, \
-    { \"x\": 23, \"y\": ${23} }, \
-    { \"x\": 24, \"y\": ${24} } ]"
+
+  # Build json string
+  #
+  # The first and last lines of the json string need to be slightly different,
+  # hence 3 if statements are used to set the string accordingly
+  for (( i = 1; i <= ${value_len}; i++ )); do
+    if [ $i -eq 1 ]; then
+      post_data+="[ { \"x\": ${i}, \"y\": ${value[i]} }, "
+    fi
+    if [ $i -gt 1 -a $i -lt ${value_len} ]; then
+      post_data+="{ \"x\": ${i}, \"y\": ${value[i]} }, "
+    fi
+    if [ $i -eq ${value_len} ]; then
+      post_data+="{ \"x\": ${i}, \"y\": ${value[i]} } ] "
+    fi
+  done
+
+  # Push the constructed json string to dashboard server
   curl -d "{ \"auth_token\": \"${AUTH_TOKEN}\", \"${DATA_VIEW}\": ${post_data} }" ${SERVER_CONNECTION}
+
 }
 
-# Pull recent values from file
-get_recent_values () {
-  value_len="24"
+get_values_from_file () {
+  # Get values from file
+
+  value_len=$1
   let i=0
-  recent_values=$( tail -n ${value_len} $SESSION_HISTORY_FILE )
-  for field in $recent_values; do
+  values=$( tail -n ${value_len} $SESSION_HISTORY_FILE )
+  for field in $values; do
     (( i++ ))  
     value[$i]=$field
   done
+
 }
 
 ## Main ##
 
-get_sabresessions
-get_recent_values
+get_tam_pool_usage
+get_values_from_file ${DATAPOINTS} # Call function with the number of recoreds to read from file
 
-# Call function and pass in array of values
-echo "values: ${values[*]}"
-update_widget ${value[*]}
+update_widget ${value[*]} # Call function with array of retrieved values
