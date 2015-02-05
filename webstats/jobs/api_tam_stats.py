202afbdfcc1d4cf68c128c9ce92afa75fe8d612e
cornellio <peter.cornell@me.com>
2015-02-04 18:47:02 -0800

    move arg parsing into a function

--- webstats/jobs/api_tam_stats.py
+++ webstats/jobs/api_tam_stats.py
@@ -16,57 +16,72 @@ import argparse
 import urllib2
 import httplib
 
-parser = argparse.ArgumentParser(
-    description='Retrieve and process Sabre TAM usage '
-    'and send to Dashing server for display')
-parser.add_argument('-d', help='Dashing server to push data to',
-                    required=False, dest='dashing_host',
-                    default='dashing.virginam.com')
-parser.add_argument('-w', help='Widget to send data to',
-                    required=False, dest='widget',
-                    default='sabresessions')
-parser.add_argument('-a', help='Authentication token '
-                    'used by Dashing server',
-                    required=False, dest='authtoken',
-                    default="mingle#trip")
-parser.add_argument('-l', help='Sabre Login Key',
-                    required=False, dest='loginkey', default='tst_key')
-parser.add_argument('-n', help='Number of data points to '
-                    'send to Dashing server, This will be the nuber of '
-                    'values shown on the x-axis of the graph',
-                    required=False, dest='num_recs', default=12)
-parser.add_argument('-i', help='Interval of data points to '
-                    'plot, where this number represents how many records '
-                    'in history file to skip when plotting data points, Allows'
-                    'plotting intervals other than default 5 minute interval',
-                    required=False, dest='num_interval', default=1)
-parser.add_argument('-f', help='Name of file to write stats to',
-                    required=False, dest='historyfile',
-                    default=sys.argv[0].strip('py') + "history_dev")
-parser.add_argument('-e', '--environment', help='Dashing environment to use, '
-                    'either "production" or "development", '
-                    'Defaults to production which uses port 80, '
-                    'Development uses port 3030',
-                    required=False, dest='dashing_env',
-                    default="production")
-parser.add_argument('--version', help='Print version and exit',
-                    required=False, action='store_true')
-
 __version__ = '0.0.1'
 __author__  = 'Pete Cornell'
 
-args          = parser.parse_args()
-auth_token    = args.authtoken
-login_key     = args.loginkey
-dashing_host  = args.dashing_host.strip('http://')
-target_widget = args.widget
-dashing_env   = args.dashing_env
-num_recs      = args.num_recs
-num_interval  = args.num_interval
-dashing_host  = "http://" + dashing_host
-DATA_VIEW     = "points"
-historyfile   = args.historyfile
-echo_version  = args.version
+
+def return_args():
+
+    parser = argparse.ArgumentParser(
+        description='Retrieve and process Sabre TAM usage '
+        'and send to Dashing server for display')
+    parser.add_argument('-d', help='Dashing server to push data to',
+                        required=False, dest='dashing_host',
+                        default='dashing.virginam.com')
+    parser.add_argument('-w', help='Widget to send data to',
+                        required=False, dest='widget',
+                        default='sabresessions')
+    parser.add_argument('-a', help='Authentication token '
+                        'used by Dashing server',
+                        required=False, dest='authtoken',
+                        default="mingle#trip")
+    parser.add_argument('-l', help='Sabre Login Key',
+                        required=False, dest='loginkey', default='tst_key')
+    parser.add_argument('-n', help='Number of data points to '
+                        'send to Dashing server, This will be the nuber of '
+                        'values shown on the x-axis of the graph',
+                        required=False, dest='num_recs', default=12)
+    parser.add_argument('-i', help='Interval of data points to '
+                        'plot, where this number represents how many records '
+                        'in history file to skip when plotting data points, '
+                        'Allows plotting intervals other than default 5 '
+                        'minute interval',
+                        required=False, dest='num_interval', default=1)
+    parser.add_argument('-f', help='Name of file to write stats to',
+                        required=False, dest='historyfile',
+                        default=sys.argv[0].strip('py') + "history_dev")
+    parser.add_argument('-e', '--environment', help='Dashing environment '
+                        'to use, either "production" or "development", '
+                        'Defaults to production which uses port 80, '
+                        'Development uses port 3030',
+                        required=False, dest='dashing_env',
+                        default="production")
+    parser.add_argument('--version', help='Print version and exit',
+                        required=False, action='store_true')
+
+    args          = parser.parse_args()
+    auth_token    = args.authtoken
+    login_key     = args.loginkey
+    dashing_host  = args.dashing_host.strip('http://')
+    target_widget = args.widget
+    dashing_env   = args.dashing_env
+    num_recs      = args.num_recs
+    num_interval  = args.num_interval
+    dashing_host  = "http://" + dashing_host
+    DATA_VIEW     = "points"
+    historyfile   = args.historyfile
+    echo_version  = args.version
+
+    return (auth_token,
+            login_key,
+            dashing_host,
+            target_widget,
+            dashing_env,
+            num_recs,
+            num_interval,
+            DATA_VIEW,
+            historyfile,
+            echo_version)
 
 
 def get_tam_usage(servers):
@@ -136,6 +151,17 @@ def check_file(file, header):
 
 def main():
 
+    (auth_token,
+        login_key,
+        dashing_host,
+        target_widget,
+        dashing_env,
+        num_recs,
+        num_interval,
+        DATA_VIEW,
+        historyfile,
+        echo_version) = return_args()
+
     if echo_version:
         print "%s version: %s " % (sys.argv[0].strip('./'), __version__)
         exit(0)
@@ -166,7 +192,7 @@ def main():
     ##
 
     ##
-    # Call functions
+    ## Call functions
     ##
 
     # get_tam_usage
