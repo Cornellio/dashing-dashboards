5826057a9bd260edd75702db51a04725ae0174f5
cornellio <peter.cornell@me.com>
2015-02-20 15:33:08 -0800

    expand support to multi environments

--- webstats/jobs/web_build_stats.py
+++ webstats/jobs/web_build_stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 '''
-Dashing job for listing website build stamps, including
+Dashing job for displaying website build info, including
 UI, API and CMS build numbers.
 '''
 
@@ -9,6 +9,7 @@ import urllib
 import httplib
 import re
 import argparse
+from argparse import RawTextHelpFormatter
 
 __version__ = '1.0.0'
 __author__  = 'Pete Cornell'
@@ -19,7 +20,7 @@ def return_args():
 
     parser = argparse.ArgumentParser(
         description='Retrieve website build stamps and send to Dashing '
-        'server for display')
+        'server for display', formatter_class=RawTextHelpFormatter)
     parser.add_argument('-d', help='Dashing server to push data to',
                         required=False, dest='dashing_host',
                         default='dashing.virginam.com')
@@ -30,6 +31,15 @@ def return_args():
     parser.add_argument('-a', help='Authentication token '
                         'used by Dashing server',
                         required=True, dest='authtoken')
+    parser.add_argument('-e', help='Web environment to get build info for\n'
+                        'Choices are:\n'
+                        'dev\n'
+                        'qa\n'
+                        'sandboxdev\n'
+                        'sandboxqa\n'
+                        'rc\n'
+                        'prod\n',
+                        dest='environment', required=False, default='prod')
 
     args          = parser.parse_args()
     auth_token    = args.authtoken
@@ -37,38 +47,77 @@ def return_args():
     dashing_port  = args.dashing_port
     target_widget = args.widget
     DATA_VIEW     = "points"
+    environment   = args.environment
 
     return (auth_token,
             dashing_host,
             dashing_port,
             target_widget,
+            environment,
             DATA_VIEW)
 
 
-def get_ui_build():
+def get_ui_build(env):
     '''Return UI Build'''
 
-    site = 'https://www.virginamerica.com/status'
+    if env == 'prod':
+        site = 'https://www.virginamerica.com/'
+    if env == 'dev':
+        site = 'http://www3.dev.virginamerica.com/status/'
+    if env == 'qa':
+        site = 'http://www3.qa.virginamerica.com/status/'
+    if env == 'sandboxdev':
+        site = 'https://sandbox.dev.virginamerica.com/status/'
+    if env == 'sandboxqa':
+        site = 'https://sandbox.qa.virginamerica.com/status/'
+    if env == 'rc':
+        site = 'https://rc.virginamerica.com/status/'
+
     url = urllib.urlopen(site)
     page_source = url.read()
     ui_build = re.findall('\w+-build-\d\d\d', page_source)
+
     return ''.join(ui_build)
 
 
-def get_api_build():
+def get_api_build(env):
     '''Return API Build'''
 
-    site = 'https://www.virginamerica.com/api/v0/release/build-stamp'
+    if env == 'prod':
+        site = 'https://www.virginamerica.com/api/v0/release/build-stamp'
+    if env == 'dev':
+        site = 'http://www3.dev.virginamerica.com/api/v0/release/build-stamp'
+    if env == 'qa':
+        site = 'http://www3.qa.virginamerica.com/api/v0/release/build-stamp'
+    if env == 'sandboxdev':
+        site = 'https://sandbox.dev.virginamerica.com/api/v0/release/build-stamp'
+    if env == 'sandboxqa':
+        site = 'https://sandbox.qa.virginamerica.com/api/v0/release/build-stamp'
+    if env == 'rc':
+        site = 'https://rc.virginamerica.com/api/v0/release/build-stamp'
+
     url = urllib.urlopen(site)
     page_source = url.read()
     api_build = re.findall('\d+', page_source)
     return ''.join(api_build)
 
 
-def get_cms_build():
+def get_cms_build(env):
     '''Return CMS Build'''
 
-    site = 'https://www.virginamerica.com/cms/version.json'
+    if env == 'prod':
+        site = 'https://www.virginamerica.com/cms/version.json'
+    if env == 'dev':
+        site = 'http://www3.dev.virginamerica.com/cms/version.json'
+    if env == 'qa':
+        site = 'http://www3.qa.virginamerica.com/cms/version.json'
+    if env == 'sandboxdev':
+        site = 'https://sandbox.dev.virginamerica.com/cms/version.json'
+    if env == 'sandboxqa':
+        site = 'https://sandbox.qa.virginamerica.com/cms/version.json'
+    if env == 'rc':
+        site = 'https://rc.virginamerica.com/cms/version.json'
+
     url = urllib.urlopen(site)
     page_source = url.read()
     cms_build = re.findall('\d+\.\d+\.\d+', page_source)
@@ -100,15 +149,16 @@ def main():
         dashing_host,
         dashing_port,
         target_widget,
+        environment,
         DATA_VIEW) = return_args()
 
     transmit_values(dashing_host,
                     dashing_port,
                     target_widget,
                     auth_token,
-                    get_ui_build(),
-                    get_api_build(),
-                    get_cms_build())
+                    get_ui_build(environment),
+                    get_api_build(environment),
+                    get_cms_build(environment))
 
 if __name__ == '__main__':
     main()
